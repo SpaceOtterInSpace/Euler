1780dd19e4a00165af33df2959e92e1d84481e05
SpaceOtterInSpace <jacqnelson@gmail.com>
2014-05-11 18:57:20 -0500

    upto
    learned how to use “unto” in ruby and replaced verbose code.

--- euler4.rb
+++ euler4.rb
@@ -3,29 +3,27 @@
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
 puts "new run"
-top = 1000
+top = 999
 bottom = 100
 pal = 0
+factor1 = 0
+factor2 = 0
 
 i = bottom
 j = bottom
 
-while i < top
-	i +=1
-	while j < top
-		j +=1
+bottom.upto(top){|i|
+	bottom.upto(top){|j|
 		prd = i * j
 		prd = prd.to_s
-		if prd == prd.reverse
-			if pal.to_i < i * j
+		if prd == prd.reverse && pal.to_i < i * j
 			pal = prd
 			factor1 = i
 			factor2 = j
-			end
 		end
-	end
-	j = bottom
-end
+	}
+}
+
 puts "palindrome: #{pal}"
 puts "factor1: #{factor1}"
 puts "factor2: #{factor2}"
