2c27a016a6713122b7415d00034011890e659d23
SpaceOtterInSpace <jacqnelson@gmail.com>
2014-05-20 09:03:34 -0500

    problem #7
    completed problem #7 and started 8

--- euler6.rb
+++ euler6.rb
@@ -1,28 +1,19 @@
-#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+#The sum of the squares of the first ten natural numbers is,
+#1^2 + 2^2 + ... + 10^2 = 385
 
-puts "****** new Run *****"
+#The square of the sum of the first ten natural numbers is,
+#(1 + 2 + ... + 10)^2 = 552 = 3025
 
-range = (1..10).to_a
+#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-#puts range
+sumsquare = 0
+top = 100
 
-def factor(div_num,factor)
-1.upto(factor){|j|
-  div_num%j ==0
- 
+1.upto(top) do |i|
+	sumsquare += i**2
+end
 
+sumdiff = (top*(top+1)/2)**2 - sumsquare 
 
-1.upto(2600){|i|
-	1.upto(10){|j|
-		if i%j !=0
-			next
-			if j==9
-				puts i
-			end
-		end
-	
-	#if i%20==0 && i%19==0 && i%18==0 && i%17==0 && i%16==0 && i%15==0 && i%14==0 && i%13==0 && i%12==0 && i %11==0 && i%10 == 0 && i%9 ==0 && i%8 ==0 && i%7==0 && i%6 ==0 && i%5==0 && i%4==0 && i%3==0 && i%2==0
-		
-	}
-}
+puts "Difference: #{sumdiff}"
--- euler7.rb
+++ euler7.rb
@@ -1,28 +1,8 @@
-#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-puts "****** new Run *****"
+#What is the 10 001st prime number?
 
-range = (1..10).to_a
+require 'Prime'
+arr = Prime.take(10001)
+puts "10,001st prime: #{arr.last}"
 
-#puts range
-
-def factor(div_num,factor)
-1.upto(factor){|j|
-  div_num%j ==0
- 
-
-
-1.upto(2600){|i|
-	1.upto(10){|j|
-		if i%j !=0
-			next
-			if j==9
-				puts i
-			end
-		end
-	
-	#if i%20==0 && i%19==0 && i%18==0 && i%17==0 && i%16==0 && i%15==0 && i%14==0 && i%13==0 && i%12==0 && i %11==0 && i%10 == 0 && i%9 ==0 && i%8 ==0 && i%7==0 && i%6 ==0 && i%5==0 && i%4==0 && i%3==0 && i%2==0
-		
-	}
-}
